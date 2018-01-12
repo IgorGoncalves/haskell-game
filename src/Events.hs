e1af2bfda9660ca0bea68ac1dd337350e897ad04
Mayara Machado <may.mchdo@gmail.com>
2018-01-12 15:41:58 -0200

    Update Events.hs

--- src/Events.hs
+++ src/Events.hs
@@ -14,4 +14,8 @@ handleEvents (EventKey (SpecialKey KeyDown) state _ _)
                 Play (Player (px,py) (pvx, pvy') (pw, ph)) cplayer ball goal cgoal
                 where
                     pvy' = if state == Down then (-80) else 0
+                    
+handleEvents (EventKey (Char 'r') _ _ _) (GameOver _ ) = initialWorld
+handleEvents (EventKey (Char 'r') _ _ _) (Play _ _ _ _ _ ) = initialWorld
+
 handleEvents _ game = game 
